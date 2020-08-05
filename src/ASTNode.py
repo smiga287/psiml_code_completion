5f488e7077f3c26076e5a474de990c9aafe8a551
Aleksandar <smiga287@gmail.com>
2020-08-05 18:32:35 +0200

    Create ASTNode class

---
+++ src/ASTNode.py
@@ -0,0 +1,10 @@
+
+class ASTNode():
+    def __init__(self, node_type, node_value, have_children: bool, have_sibling: bool):
+        self.type = node_type
+        self.value = node_value
+        self.have_children = have_children
+        self.have_sibling = have_sibling
+    
+    def __eq__(self, other):
+        return self.type == other.type and self.value == other.value and self.have_children == other.have_children and self.have_sibling == other.have_sibling
