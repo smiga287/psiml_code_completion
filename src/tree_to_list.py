bee276bb47748af1caaf06c87873b87f1e35e638
djovak <novakzivanic@gmail.com>
2020-08-05 17:02:00 +0000

    djovakov prvi commit

---
+++ src/tree_to_list.py
@@ -0,0 +1,21 @@
+import json
+import sys
+
+niz = None
+
+
+def dfs(index, hasparent):
+    pass
+
+
+def run():
+    with open('data\python100k_train.json') as json_file:
+        str = json_file.read().split('\n')
+        str = str[:-1]
+        niz = [json.loads(str[0])]
+        dfs(niz[0], False, )
+
+
+
+if __name__ == "__main__":
+    run()
