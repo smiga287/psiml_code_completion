ff5887e0f40a6ab0daa158376bf1f1450c68df4d
Aleksandar <smiga287@gmail.com>
2020-08-07 15:52:43 +0000

    Refactor preprocessing

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,6 +2,7 @@ import json
 import pickle
 import sys
 import random
+import os.path
 from collections import Counter
 from JSONToVector import JSONToVector
 
@@ -9,45 +10,37 @@ from JSONToVector import JSONToVector
 def preprocess_dataset(dataset, name):
     json_to_vec = JSONToVector(*dataset)
     json_to_vec.solve()
-    json_to_vec.export_pickle()
-    serialize_dicts(get_tag_dicts(), name)
+    if not vector_export_exists(name):
+        json_to_vec.export_pickle()
+    if not dict_export_exists(name, 'tag'):
+        serialize_dicts(create_tag_dicts(json_to_vec.res), name, 'tag')
+    if not dict_export_exists(name, 'val'):
+        serialize_dicts(create_val_dicts(json_to_vec.res), name, 'val')
 
 
-def preprocess_large_datasets():
-    python_50k_eval = ("python50k_eval.json", "python50k_eval_vector.pickle")
-    python_100k_test = ("python100k_train.json", "python100k_train_vector.pickle")
 
-    for dataset, name in [
-        (python_50k_eval, "python50k"),
-        (python_100k_test, "python100k"),
-    ]:
-        preprocess_dataset(dataset, name)
+def vector_export_exists(name):
+    return os.path.exists(f'D://data//{name}_vector.pickle')
+
+
+def dict_export_exists(name, obj_type):
+    obj_to_idx, idx_to_obj = get_dict_names(name, obj_type)
+    return os.path.exists(f'D://data//{obj_to_idx}.pickle') and os.path.exists(f'D://data//{idx_to_obj}.pickle') 
+
+
+def preprocess_train_datasets():
+    python_100k_train = ("python100k_train.json", "python100k_vector.pickle")
+    preprocess_dataset(python_100k_train, 'python100k')
+
+
+def preprocess_eval_datasets():
+    python_50k_eval = ("python50k_eval.json", "python50k_vector.pickle")
+    preprocess_dataset(python_50k_eval, 'python50k')
 
 
 def preprocess_small_dataset():
     python_1k = ("python1k.json", "python1k_vector.pickle")
-    preprocess_dataset(python_1k, "python_1k")
-
-
-def get_dict_names(filename):
-    return f"{filename}_tag_to_idx", f"{filename}_idx_to_tag"
-
-
-def serialize_dicts(dicts, filename):
-    names = get_dict_names(filename)
-    for d, name in zip(dicts, names):
-        with open(f"D://data//{name}.pickle", "wb") as f:
-            pickle.dump(d, f)
-
-
-def get_tag_dicts(filename="python100k"):
-    tag_to_idx_path, idx_to_tag_path = map(
-        lambda name: f"{name}.pickle", get_dict_names(filename)
-    )
-    with open(f"D://data//{tag_to_idx_path}", "rb") as tti_file, open(
-        f"D://data//{idx_to_tag_path}", "rb"
-    ) as itt_file:
-        return pickle.load(tti_file), pickle.load(itt_file)
+    preprocess_dataset(python_1k, "python1k")
 
 
 def create_tag_dicts(vector):
@@ -64,11 +57,17 @@ def create_tag_dicts(vector):
     return tag_to_idx, idx_to_tag
 
 
-def calc_num_of_most_freq_vals(vector):
-    return 1200
+def get_tag_dicts(filename="python100k"):
+    tag_to_idx_filename, idx_to_tag_filename = map(
+        lambda name: f"{name}.pickle", get_dict_names(filename, 'tag')
+    )
+    with open(f"D://data//{tag_to_idx_filename}", "rb") as tti_file, open(
+        f"D://data//{idx_to_tag_filename}", "rb"
+    ) as itt_file:
+        return pickle.load(tti_file), pickle.load(itt_file)
 
 
-def create_value_dicts(vector):
+def create_val_dicts(vector):
     val_to_idx = {}
     idx_to_val = {}
 
@@ -85,6 +84,31 @@ def create_value_dicts(vector):
         idx += 1
 
     return val_to_idx, idx_to_val
+
+
+def calc_num_of_most_freq_vals(vector):
+        return 1200
+
+
+def get_val_dicts(filename="python100k"):
+    val_to_idx_filename, idx_to_val_filename = map(
+        lambda name: f"{name}.pickle", get_dict_names(filename, 'val')
+    )
+    with open(f"D://data//{val_to_idx_filename}", "rb") as vti_file, open(
+        f"D://data//{idx_to_val_filename}", "rb"
+    ) as itv_file:
+        return pickle.load(vti_file), pickle.load(itv_file)
+
+
+def get_dict_names(filename, obj_type):
+    return f"{filename}_{obj_type}_to_idx", f"{filename}_idx_to_{obj_type}"
+
+
+def serialize_dicts(dicts, filename, obj_type):
+    names = get_dict_names(filename, obj_type)
+    for d, name in zip(dicts, names):
+        with open(f"D://data//{name}.pickle", "wb") as f:
+            pickle.dump(d, f)
 
 
 def create_small_dataset():
@@ -112,5 +136,5 @@ def get_eval_dataset():
 
 
 if __name__ == "__main__":
-    vti, itv = create_value_dicts(get_small_dataset())
+    a, b = get_val_dicts()
     print()
