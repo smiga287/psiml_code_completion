c52ec03e1db1d83bfb764352c54d9bca7fdd3859
Aleksandar <smiga287@gmail.com>
2020-08-07 20:07:19 +0200

    Create value LSTM

--- src/LSTMTagger.py
+++ src/LSTMTagger.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class LSTMTagger(nn.Module):
 
-    def __init__(self, embedding_dim, hidden_dim , vocab_size, tagset_size, layer_cnt):
+class LSTMTagger(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size, layer_cnt):
         super(LSTMTagger, self).__init__()
         self.hidden_dim = hidden_dim
 
@@ -13,14 +13,14 @@ class LSTMTagger(nn.Module):
         self.h_0 = torch.nn.Parameter(torch.randn(hidden_dim))
         self.c_0 = torch.nn.Parameter(torch.randn(embedding_dim))
 
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True, num_layers=layer_cnt)
-        
-
+        self.lstm = nn.LSTM(
+            embedding_dim, hidden_dim, batch_first=True, num_layers=layer_cnt
+        )
         self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
 
     def forward(self, sentence):
         embeds = self.word_embeddings(sentence.long())
         lstm_out, _ = self.lstm(embeds)
-        tag_space = self.hidden2tag(lstm_out[:,-1,:])
+        tag_space = self.hidden2tag(lstm_out[:, -1, :])
         tag_scores = F.log_softmax(tag_space, dim=1)
         return tag_scores
