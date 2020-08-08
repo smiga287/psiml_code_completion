890321031ca61ddb60517df8910b88dda5ab20b2
Novak Zivanic <38144124+novak2000@users.noreply.github.com>
2020-08-08 15:20:43 +0200

    update atentionmodel

--- src/AtentionModel.py
+++ src/AtentionModel.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 class LSTMTagger(nn.Module):
 
-    def __init__(self,tag_model, vocab_size_tag, vocab_size_val, embedding_dim_tag, embedding_dim_val, hidden_dim , vocab_size, tagset_size, layer_cnt):
+    def __init__(self,tag_model, vocab_size_tag, vocab_size_val, embedding_dim_tag, embedding_dim_val, hidden_dim , layer_cnt):
         super(LSTMTagger, self).__init__()
         self.hidden_dim = hidden_dim
 
@@ -22,24 +22,32 @@ class LSTMTagger(nn.Module):
 
         self.lstm = nn.LSTM( self.embedding_dim, hidden_dim, batch_first=True, num_layers=layer_cnt)
         
-        self.wm = nn.Linear(hidden_dim, hidden_dim)
-        self.wh = nn.Linear(hidden_dim, hidden_dim)
+        self.wm = nn.Linear(hidden_dim, hidden_dim, bias=False)
+        self.wh = nn.Linear(hidden_dim, hidden_dim, bias=False)
 
-        self.v = nn.Linear(hidden_dim, 1)
+        self.v = nn.Linear(hidden_dim, 1, bias=False)
 
-        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+        self.wg = nn.Linear(2*hidden_dim, hidden_dim, bias=False)
+        self.final_tag = nn.Linear(hidden_dim, vocab_size_tag)
+        self.final_val = nn.Linear(hidden_dim, vocab_size_val)
+
 
     def forward(self, sentence):
         embeds = torch.cat( (self.tag_embeddings(sentence[:,:,0]), self.val_embeddings(sentence[:,:,1])), 2)
         lstm_out, (last_h, last_c) = self.lstm(embeds)
         last_h.squeeze_()
         mt = lstm_out[:, :-1, :]
-        At =  self.v( self.wm(mt) + self.wh(last_h.unsqueeze_(dim=1)) )
+        At =  self.v( F.tanh(self.wm(mt) + self.wh(last_h.unsqueeze_(dim=1))) )
 
         alfa = F.softmax(At)
 
-        ct = torch.matmul((mt))
+        ct = torch.matmul(mt,alfa).squeeze()
 
-        tag_space = self.hidden2tag(mt)
-        tag_scores = F.log_softmax(tag_space, dim=1)
+        Gt = self.wg(torch.cat((last_h, ct),1))
+        Gt = F.tanh(Gt)
+
+        tag = self.final_tag(Gt)
+        val = self.final_val(Gt)
+        tag = F.log_softmax(tag, dim=1)
+        val = F.log_softmax(val, dim=1)
         return tag_scores
